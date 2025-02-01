23e89a561a63b4a4cf69976087e9dae76ef7f070
wanderlust-li <zhekalichm4n@icloud.com>
2025-02-01 15:40:39 +0200

    remove comments

--- TaskList.Infrastructure/InfrastructureServicesRegistration.cs
+++ TaskList.Infrastructure/InfrastructureServicesRegistration.cs
@@ -13,17 +13,14 @@ namespace TaskList.Infrastructure
         public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
             IConfiguration configuration)
         {
-            // Отримуємо налаштування MongoDB з конфігурації (appsettings.json знаходиться в проекті API)
             services.Configure<MongoDBSettings>(options =>
                 configuration.GetSection("MongoDBSettings").Bind(options));
 
             services.AddSingleton(sp =>
                 sp.GetRequiredService<IOptions<MongoDBSettings>>().Value);
-                
-            // Реєструємо MongoDBContext як синглтон
+            
             services.AddSingleton<MongoDBContext>();
-                
-            // Реєструємо репозиторій
+            
             services.AddScoped<ITaskListRepository, TaskListRepository>();
                 
             return services;
